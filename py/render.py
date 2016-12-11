3cd1e965ccbaa8896e86d8364628d50078585d2d
Jakub Turski <yacoob@gmail.com>
2016-12-11 23:07:01 +0000

    Some rendering adjusted to new data.

--- py/render.py
+++ py/render.py
@@ -35,12 +35,17 @@ class Renderer(object):
         def datetimeToISO(dt):
             return dt.isoformat()
 
+        def datetimeToShortDate(dt):
+            return dt.strftime('%Y-%m-%d')
+
+
         self.jinja = jinja2.Environment(
                 trim_blocks=True, lstrip_blocks=True,
                 loader=jinja2.FileSystemLoader(d))
         self.jinja.globals['base_url'] = BASE_URL
         self.jinja.filters['datetime_to_iso'] = datetimeToISO
         self.jinja.filters['datetime_to_rfc822'] = datetimeToRFC822
+        self.jinja.filters['short_date'] = datetimeToShortDate
         self.jinja.filters['fancy_grade'] = fancyGradeFilter
 
     def render(self, fn, tmpl_fn=None, **kwargs):
@@ -78,42 +83,26 @@ def renderEverything(d=None, gcu=None):
             os.path.join(outdir, grade, 'index.html'),
             tmpl_fn='grade.j2', gcu=gcu, grade=grade)
         # for every kit:
-        for (kit, posts) in kits.items():
+        for kit in kits:
             # render a kit page with all entries
             r.render(
-                os.path.join(outdir, grade, kit, 'index.html'),
+                os.path.join(outdir, grade, kit['slug'], 'index.html'),
                 tmpl_fn='kit.j2', gcu=gcu, grade=grade, kit=kit)
-            latest_kit_post_date = max(
-                    [gcu['posts'][x]['meta']['date'] for x in posts])
             sitemap_urls.append((
-                '/%s/%s/' % (grade, kit), latest_kit_post_date))
+                '/%s/%s/' % (grade, kit), kit['last_updated']))
         latest_grade_post_date = max(
                 [x[1] for x in sitemap_urls
                     if x[0].startswith('/%s/' % grade)])
         sitemap_urls.append(('/%s/' % grade, latest_grade_post_date))
 
     # generate special pages
-    last_update_date = max(x['meta']['date'] for x in gcu['posts'].values())
-    for (fn, tmpl) in SPECIAL_PAGES.items():
-        r.render(
-            os.path.join(outdir, fn),
-            tmpl_fn=tmpl, gcu=gcu, last_update_date=last_update_date,
-            sitemap_urls=sitemap_urls)
-    for (fn, url) in SPECIAL_REDIRECTS.items():
-        r.render(
-            os.path.join(outdir, fn),
-            tmpl_fn='redir.j2', url=url)
-
-    # for every loaded post:
-    for (post_key, post) in gcu['posts'].items():
-        m = post['meta']
-        # render single post redirects
-        r.render(
-            os.path.join(outdir, m['date'].strftime('%Y/%m/%d'), m['slug'],
-                         'index.html'),
-            tmpl_fn='redir.j2', url=post['redir_url'])
-        # render aliases
-        for a in m.get('aliases', ()):
-            r.render(
-                os.path.join(outdir, a.strip('/'), 'index.html'),
-                tmpl_fn='redir.j2', url=post['redir_url'])
+    # last_update_date = max(x['meta']['date'] for x in gcu['posts'].values())
+    # for (fn, tmpl) in SPECIAL_PAGES.items():
+    #     r.render(
+    #         os.path.join(outdir, fn),
+    #         tmpl_fn=tmpl, gcu=gcu, last_update_date=last_update_date,
+    #         sitemap_urls=sitemap_urls)
+    # for (fn, url) in SPECIAL_REDIRECTS.items():
+    #     r.render(
+    #         os.path.join(outdir, fn),
+    #         tmpl_fn='redir.j2', url=url)
