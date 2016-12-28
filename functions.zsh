f56bca2aa57b06d9a58cabcf80f3f406a8d4cb52
Jakub Turski <yacoob@gmail.com>
2016-12-28 20:45:40 +0000

    Update helper functions for new photo layout. Things got simpler!

--- functions.zsh
+++ functions.zsh
@@ -1,84 +1,51 @@
-# Reads a feed of newest photos from Google Photos, outputs yaml data for the
-# gallery. Can run wherever, expects the feed on stdin.
-#
-# gcu-gallery 12 < feedfile
-#
+# Helper variable.
+GCU_ROOT_DIR=$(git rev-parse --show-toplevel)
+
+# Generate YAML bit for last N photos.
 gcu-gallery() {
-echo '  photos:'
-  grep -Eo "https?://lh.\.googleusercontent.com/[^']+/s144/[^']+" - | head -${1} | sed -E '
-s#/s144/#/s1920/#
-s/https?://
-s/^/  - href: /
-' | tail -r
-}
-
-# Extracts Google Photos urls from files, creates per date lists of URLs. Must
-# run in photos/, expects kit files as arguments.
-#
-# gcu-make-list ../kits/hg/exia.yaml
-#
-gcu-make-list() {
-  foreach file ("$@") {
-    local new_content=$(git diff --cached -U0 ${file})
-    local day=$(echo "${new_content}" | grep 'date: ' | grep -Eo '\d\d\d\d-\d\d-\d\d')
-    mkdir -p ${day}
-    echo "${new_content}" | grep -Eo '//lh[0-9]\.googleusercontent.+/s1920/[^"]+' | sort -u | sed 's/^/http:/' >> ${day}/urllist
-    echo ${day}
+  local last=${$(basename $(ls ${GCU_ROOT_DIR}/photo/full | tail -1))%.*}
+  echo '  photos:'
+  foreach n ($(seq -f %05g $last -1 $(($last - ${1} + 1)))) {
+    echo "  - href: //syn.tactical-grace.net/f/full/$n.jpg"
   }
 }
 
-# Given a list of photos, fetches all needed variants of it. Must run in
-# photo/, expects dir names (YYYY-MM-DD) as arguments.
-#
-# gcu-fetch-day 2016-12-26
-#
-gcu-fetch-day() {
-  foreach day ("$@") {
-    local photodir=$(pwd)
-    local daydir=${photodir}/${day}
-    foreach dir (s1920 s400-p) {
-      cd ${daydir}
-      mkdir -p ${dir}
-      cd ${dir}
-      sed "s#/s1920/#/${dir}/#" ../urllist | xargs -- curl -s --remote-name-all
+# Update thumbnails for selected photos.
+gcu-update-thumbs() {
+  local -a list
+  local photos=(${GCU_ROOT_DIR}/photo/full/*jpg)
+  if [[ "${1}" =~ "[0-9]+" ]] {
+    list=($photos[-${1},-1])
+  } elif [[ "${1}" == "-f" ]] {
+    list=(${photos})
+  } else {
+    foreach file ($photos[@]) {
+      local sn=$(basename ${file})
+      if [[ ${file} -nt ${GCU_ROOT_DIR}/photo/thumb/${sn} ]] {
+        list+=${file}
+      }
     }
-    cd ${photodir}
   }
+  foreach file ($list[@]) {
+    local thumbfile=${GCU_ROOT_DIR}/photo/thumb/$(basename ${file})
+    smartcroppy --width 400 --height 400 ${file} ${thumbfile} && ls -l ${thumbfile}
+  }
+
 }
 
-# Syncs photos to serving site. Must run in photo/, must have photos for *all*
-# days already fetched (as it runs 'rsync --delete').
-#
-# gcu-sync-photos -n
-#
+# Sync photo dir to the serving site.
 gcu-sync-photos() {
-  rsync --exclude .htaccess --exclude .gitignore --delete -airz $* . inferno.hell.pl:/srv/web/syn.tactical-grace.net/c/
+  rsync --exclude .htaccess --exclude .gitignore \
+        --delete -airz $* \
+        $GCU_ROOT_DIR/photo/ \
+        inferno.hell.pl:/srv/web/syn.tactical-grace.net/f/
 }
 
-# Rewrites image links in kit files from Google Photos to serving site.
-# Can run wherever, expects kit files as arguments.
-#
-# gcu-rewrite-links ../kits/hg/exia.yaml
-#
-gcu-rewrite-links() {
-  local base='//syn.tactical-grace.net/c/'
-  foreach file ("$*") {
-    local day=$(git diff --cached -U0 ${file} | grep 'date: ' | grep -Eo '\d\d\d\d-\d\d-\d\d')
-    local new_parent=${base}${day}
-    perl -i -lpe 's#//lh[0-9]\.googleusercontent.+(?=/s[0-9]+[^/]+/)#'${new_parent}'#' ${file}
-  }
-}
-
-# Convenience function for new entries: extracts list of photos, fetches them
-# and edits the links. Doesn't sync the photos to the serving site.
-#
-# gcu-process-photos ../kits/hg/exia.yaml
+# Convenience function. No sync, though.
 gcu-process-photos() {
-  foreach file ("$@") {
-    local day=$(gcu-make-list ${file})
-    ls -l ${day}
-    gcu-fetch-day ${day}
-    find ${day}
-    gcu-rewrite-links ${file}
+  if [[ "${1}" =~ "[0-9]+" ]] {
+    gcu-gallery ${1}
+    gcu-update-thumbs ${1}
+    gcu-sync-photos -n
   }
 }
