e920546061bb268bfa38955a2a3d454a1b614dc7
Jakub Turski <yacoob@gmail.com>
2019-04-11 23:53:26 +0100

    First stab at using non-jQ lightgallery.

--- static/js/gcu.js
+++ static/js/gcu.js
@@ -1,11 +1,11 @@
 var gcu = gcu || {
   hashPrefix: 'p/',
   dateHashPrefix: /\d\d\d\d-\d\d-\d\d/,
+  // FIXME: captions seem to be gone :(
   lgOptions: {
     hideBarsDelay: 2000,
     keyPress: true,
     selector: '.gallery',
-    showThumbByDefault: false,
     speed: 200,
     youtubePlayerParams: {
       autoplay: 0,
@@ -51,53 +51,64 @@ gcu.postPageHandler = function() {
   /* Set up kit page.
    */
   // Enable lightbox.
-  var lg = $('.container');
-  lg.lightGallery(gcu.lgOptions);
+  var lg = document.querySelector('.container');
+  lightGallery(lg, gcu.lgOptions);
   // Set helper vars.
-  gcu.lg_data = lg.data('lightGallery');
-  var gallery_elements = $('a' + gcu.lgOptions.selector);
+  gcu.lg_data = window.lgData[lg.getAttribute('lg-uid')];
   // Bind lightbox events.
-  lg.on('onAfterSlide.lg', function(event, prevIndex, index, fromTouch, fromThumb) {
-    gcu.setHashIdx(index + 1);
+  lg.addEventListener('onAfterSlide', function(event) {
+    gcu.setHashIdx(event.detail.index + 1);
   });
-  lg.on('onCloseAfter.lg', function(event) {
+  lg.addEventListener('onCloseAfter', function(event) {
     gcu.setHashIdx('');
   });
   // Inhibit hashchange-triggered updates to avoid double updates when user
   // clicks on the a.
-  gallery_elements.click(function() {
-    gcu.inhibitHashChange = true;
+  var gallery_selector = 'a' + gcu.lgOptions.selector;
+  var gallery_elements = document.querySelectorAll(gallery_selector);
+  gallery_elements.forEach(function (item, idx) {
+    item.addEventListener('click', function() {
+      gcu.inhibitHashChange = true;
+    });
   });
+  // We'll be clicking on things.
+  // FIXME: verify whether this behaves as expected (eg. doesn't actually open
+  // the image).
+  var click = new MouseEvent('click', { view: window, bubbles: true, cancelable: true});
   // Handle hashchange event (user typing, history navigation, etc.)
-  $(window).bind('hashchange', function() {
+  window.addEventListener('hashchange', function () {
     var idx = gcu.getHashIdx();
     if (!gcu.inhibitHashChange) {
       if (idx > 0 && idx <= gallery_elements.length) {
         if (gcu.lg_data.lGalleryOn) {
           gcu.lg_data.slide(idx - 1);
         } else {
-          gallery_elements.eq(idx - 1).trigger('click');
+          gallery_elements[idx - 1].dispatchEvent(click);
         }
       } else {
         gcu.lg_data.destroy();
       }
     }
     gcu.inhibitHashChange = false;
-  });
+  }, false);
   // If a date hash was set, bring up lightbox for the first photo of that day.
-  var hash_string = location.hash.substr(1);
-  if (hash_string.match(gcu.dateHashPrefix)) {
-    var first_photo_of_day = $('#' + hash_string).parent().next().find('a.gallery').first();
-    if (first_photo_of_day) {
-      var pos = gallery_elements.index(first_photo_of_day);
-      if (pos >= 0) {
-        gallery_elements.eq(pos).trigger('click');
-      }
-    }
-  }
-  // If a photo has was set, bring up lightbox for that photo.
-  var idx = gcu.getHashIdx();
-  if (idx > 0 && idx <= gallery_elements.length) {
-    gallery_elements.eq(idx - 1).trigger('click');
-  }
+  // var hash_string = location.hash.substr(1);
+  // if (hash_string.match(gcu.dateHashPrefix)) {
+  //   var first_photo_of_day = document.getElementById(hash_string).parentNode.nextElementSibling.querySelector(gallery_selector);
+  //   if (first_photo_of_day) {
+  //     var pos = -1;
+  //     for (var i=0; i<gallery_elements.length; i++) {
+  //       if (gallery_elements[i] == first_photo_of_day) {
+  //       pos = i;
+  //     }}
+  //     if (pos >= 0) {
+  //       gallery_elements[pos].dispatchEvent(click);
+  //     }
+  //   }
+  // }
+  // // If a photo has was set, bring up lightbox for that photo.
+  // var idx = gcu.getHashIdx();
+  // if (idx > 0 && idx <= gallery_elements.length) {
+  //   gallery_elements[idx - 1].dispatchEvent(click);
+  // }
 };
