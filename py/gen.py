4f521abea4975462e28ca08e0b8f276509f2ed5d
Jakub Turski <yacoob@gmail.com>
2019-05-27 17:43:05 +0100

    Redo cli handling.
    There are now two commands - `build` and `serve`. `build` can be asked
    to produce the output in an arbitrary directory plus it can optionally
    skip pre-seeding static to it. `serve` works the same way as before:
    generate full site in `public/`, start serving it, regen on demand.

--- py/gen.py
+++ py/gen.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
+import argparse
 import datetime
 import os
-import sys
 import threading
 import traceback
 import time
@@ -33,10 +33,12 @@ def reloadData():
     dataLock.release()
 
 
-def renderSite():
+def renderSite(outdir=None, skip_static=False):
+    if not outdir:
+        raise ValueError('output directory needs to be set, is %s' % outdir)
     dataLock.acquire()
     _tsprint('rendering site')
-    render.renderEverything(WORKDIR, gcu_data)
+    render.renderEverything(WORKDIR, gcu_data, outdir, skip_static)
     _tsprint('site rendered')
     dataLock.release()
 
@@ -45,7 +47,8 @@ class GCUFileChangedHandler(events.PatternMatchingEventHandler):
     """File event handler for fs watcher. Watches files only, on event
     optionally reloads GCU data and rerenders everything."""
 
-    def __init__(self, reload_data=False, **kwargs):
+    def __init__(self, reload_data=False, outdir=None, **kwargs):
+        self.outdir = outdir
         self.reload_data = reload_data
         super(GCUFileChangedHandler, self).__init__(**kwargs)
 
@@ -54,35 +57,40 @@ class GCUFileChangedHandler(events.PatternMatchingEventHandler):
         try:
             if self.reload_data:
                 reloadData()
-            renderSite()
+            renderSite(self.outdir)
         except Exception:
             traceback.print_exc()
             os._exit(1)
 
 
-def serve():
-    os.chdir(os.path.join(WORKDIR, 'public'))
+def build(output_dir, skip_static):
+    reloadData()
+    renderSite(output_dir, skip_static)
+
+
+def serve_cmd(args):
+    outdir = os.path.join(WORKDIR, 'public')
+    build(outdir, False)
+    os.chdir(outdir)
     server = BaseHTTPServer.HTTPServer(
         ('', 8000), SimpleHTTPServer.SimpleHTTPRequestHandler)
     thread = threading.Thread(target=server.serve_forever)
     thread.daemon = True
     thread.start()
-
-
-def watch():
-    once()
-    serve()
     observer = observers.Observer()
     observer.schedule(GCUFileChangedHandler(patterns=('*.yaml', ),
                                             ignore_patterns=IGNORED_FILES,
+                                            outdir=outdir,
                                             reload_data=True),
                       os.path.join(WORKDIR, 'kits'),
                       recursive=True)
     observer.schedule(GCUFileChangedHandler(patterns=('*.j2', ),
-                                            ignore_patterns=IGNORED_FILES),
+                                            ignore_patterns=IGNORED_FILES,
+                                            outdir=outdir),
                       os.path.join(WORKDIR, 'templates'),
                       recursive=True)
-    observer.schedule(GCUFileChangedHandler(ignore_patterns=IGNORED_FILES),
+    observer.schedule(GCUFileChangedHandler(ignore_patterns=IGNORED_FILES,
+                                            outdir=outdir),
                       os.path.join(WORKDIR, 'static'),
                       recursive=True)
     observer.start()
@@ -94,27 +102,23 @@ def watch():
     observer.join()
 
 
-def once():
-    reloadData()
-    renderSite()
-
-
-def print_usage():
-    print """Usage: %s COMMAND [ARGS]
-Commands:
-    build [outputdir] - renders site to outputdir ('public/' by default)
-    serve             - renders site, serves it locally, updates when input
-                        changes
-""" % sys.argv[0]
+def build_cmd(args):
+    build(args.output_dir, args.skip_static)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if sys.argv[1] == 'serve':
-            watch()
-        elif sys.argv[1] == 'build':
-            once()
-        else:
-            print_usage()
-    else:
-        print_usage()
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+    parser_build = subparsers.add_parser('build', help='render site')
+    parser_build.add_argument('--output_dir',
+                              default='public',
+                              help='custom output directory (default: public)')
+    parser_build.add_argument('--skip_static',
+                              action='store_true',
+                              help="don't copy static/ to the outputdir")
+    parser_build.set_defaults(func=build_cmd)
+    parser_serve = subparsers.add_parser(
+        'serve', help='serve rendered site locally, update when input changes')
+    parser_serve.set_defaults(func=serve_cmd)
+    args = parser.parse_args()
+    args.func(args)
