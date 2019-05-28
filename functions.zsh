9ae85bbe12ecfe6d1a25dfe1484af2fd30322d94
Jakub Turski <yacoob@gmail.com>
2019-05-29 00:09:31 +0100

    Update shell helper functions.

--- functions.zsh
+++ functions.zsh
@@ -1,10 +1,11 @@
 # Helper variable.
 GCU_ROOT_DIR=$(git rev-parse --show-toplevel)
+PHOTO_DIR=${GCU_ROOT_DIR}/static/photos
 
 # Generate YAML bit for last N photos.
 gcu-gallery() {
   local prefix='gcu-'
-  local last=${$(basename $(ls ${GCU_ROOT_DIR}/photo/full | tail -1))%.*}
+  local last=${$(basename $(ls ${PHOTO_DIR}/full | tail -1))%.*}
   last=${last#${prefix}}
   echo '  photos:'
   foreach n ($(seq -f %05g $(($last - ${1} + 1)) ${last})) {
@@ -15,8 +16,7 @@ gcu-gallery() {
 # Update thumbnails for selected photos.
 gcu-update-thumbs() {
   local -a list
-  local photodir=${GCU_ROOT_DIR}/photo
-  local photos=(${photodir}/full/*jpg)
+  local photos=(${PHOTO_DIR}/full/*jpg)
   if [[ "${1}" =~ "[0-9]+" ]] {
     list=($photos[-${1},-1])
   } elif [[ "${1}" == "-f" ]] {
@@ -24,16 +24,16 @@ gcu-update-thumbs() {
   } else {
     foreach file ($photos[@]) {
       local sn=$(basename ${file})
-      if [[ ${file} -nt ${photodir}/thumb/${sn} ]] {
+      if [[ ${file} -nt ${PHOTO_DIR}/thumb/${sn} ]] {
         list+=${file}
       }
     }
   }
   foreach file ($list[@]) {
-    local thumbfile=${photodir}/thumb/$(basename ${file})
-    smartcroppy --width 400 --height 400 ${file} ${thumbfile} && ls -l ${thumbfile}
+    local thumbfile=${PHOTO_DIR}/thumb/$(basename ${file})
+    pipenv run smartcroppy --width 400 --height 400 ${file} ${thumbfile} && ls -l ${thumbfile}
   }
-  chmod -R a+rX ${photodir}
+  chmod -R a+rX ${PHOTO_DIR}
 }
 
 # Convenience function.
