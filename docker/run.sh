10beabdc43a47ea24012715c9e234e0e025da5d0
Jakub Turski <yacoob@gmail.com>
2020-10-30 00:35:03 +0000

    Set ssh-related stuff in the container.

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 docker run -it --rm \
   --hostname gcu-dev \
-  #-v /home/yacoob/.netlify:/home/yacoob/.netlify \
-  #-v /home/yacoob/workarea/gcu:/home/yacoob/workarea/gcu \
+  -p 1111:1111 \
+  -v /home/yacoob/.netlify:/home/yacoob/.netlify \
+  -v /home/yacoob/.ssh:/home/yacoob/.ssh:ro \
+  -e SSH_AUTH_SOCK=/home/yacoob/.ssh/agent.sock \
   yacoob/gcu-dev
+  #-v /home/yacoob/workarea/gcu:/home/yacoob/workarea/gcu \
