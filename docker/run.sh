ba901c8442e666ba71d806dbf665bf064812c5ae
Jakub Turski <yacoob@gmail.com>
2020-11-04 02:01:01 +0000

    Forward an entire ~/.ssh into the container.

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 DOCKER_IMAGE=${DOCKER_IMAGE:-gcu-dev}
 netlify_config=~/.config/netlify
-ssh_socket=${SSH_AUTH_SOCK:-~/.ssh/agent.sock}
+ssh_dir=~/.ssh
+ssh_socket=${SSH_AUTH_SOCK:-${ssh_dir}/agent.sock}
 workdir=~/workarea/gcu
 if [ -d ${workdir} ]; then
   workdir_opt="-v ${workdir}:${workdir}"
@@ -12,7 +13,7 @@ docker run -it --rm \
   --hostname ${DOCKER_IMAGE} \
   -p 1111:1111 \
   -v ${netlify_config}:${netlify_config} \
-  -v ${ssh_socket}:${ssh_socket} \
+  -v ${ssh_dir}:${ssh_dir} \
   -e SSH_AUTH_SOCK=${ssh_socket} \
   ${workdir_opt} \
   yacoob/${DOCKER_IMAGE}
