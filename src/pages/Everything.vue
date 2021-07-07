7536430ee654fb3fc5aa14c82b4b8d33beb2b113
Jakub Turski <yacoob@gmail.com>
2021-07-07 01:20:31 +0100

    Quick'n'dirty /everything.

--- src/pages/Everything.vue
+++ src/pages/Everything.vue
@@ -1,3 +1,29 @@
 <template>
-  <h1>All kits made by GCU.</h1>
+  <div>
+    <h1>All kits made by GCU.</h1>
+    <ul>
+      <li v-for="edge in $page.allKit.edges" :key="edge.node">
+        <g-link :to="edge.node.path"
+          >{{ edge.node.grade.title }}: {{ edge.node.title }}</g-link
+        >
+      </li>
+    </ul>
+  </div>
 </template>
+
+<page-query>
+query allAvailableKits {
+  allKit(sort: [{by: "grade", order: ASC}, {by: "title", order: ASC}]) {
+    edges {
+      node {
+        path
+        title
+        grade {
+          title
+        }
+      }
+    }
+  }
+}
+
+</page-query>
