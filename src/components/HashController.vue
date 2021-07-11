d968a76e8e1a3ca932405c00f522f4c1287112b5
Jakub Turski <yacoob@gmail.com>
2021-07-11 17:56:14 +0100

    Improve hash->photo number logic.

--- src/components/HashController.vue
+++ src/components/HashController.vue
@@ -44,22 +44,33 @@ export default {
     },
   },
   methods: {
+    // Handle url hash.
     parseUrlAndUpdateHash: function () {
-      // Handle url fragment.
       const dateRe = /#\d\d\d\d-\d\d-\d\d/;
-      const photoRe = /#photo\d+/;
+      const photoRe = /#photo(\d+)/;
       const hash = location.hash;
+      if (hash) {
+        // If we determine a photo number from the hash, we will emit a signal
+        // that page should move.
       let targetPhoto = this.currentPhoto;
       if (dateRe.test(hash)) {
+          // There's an YYYY-MM-DD date in the hash.
         console.log("it's a date! I should totally do something about it...");
       } else if (photoRe.test(hash)) {
+          // There's a photo indicator in the hash.
         console.log("it's a photo number!");
-        const n = Number(hash.slice(6));
+          const n = Number(hash.match(photoRe)[1]);
+          // Check whether requested photo is within expected range.
         if (n >= 1 && n <= this.photoCount) {
           targetPhoto = n - 1;
+          } else {
+            targetPhoto = null;
         }
+        } else {
+          // There was a hash, but I couldn't parse it.
+          targetPhoto = null;
       }
-      if (targetPhoto != this.currentPhoto) {
+        // Inform parent about new photo that we've worked out from the hash.
         this.$emit("gallery-moved-to", targetPhoto);
       }
     },
