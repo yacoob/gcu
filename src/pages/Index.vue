2ea4465cb5a37151dd0277be56fe2638411abd8e
Jakub Turski <yacoob@gmail.com>
2021-07-11 19:24:40 +0100

    Main page: now with actual new entries!

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,23 +1,46 @@
 <template>
   <Layout>
+    <h1>🤖😺😻😽</h1>
     <ul>
-      <li>
-        <g-link to="/rg/aile-strike/">RG Aile Strike: multi-entry kit</g-link>
-      </li>
-      <li>
-        <g-link to="/hg/zee-zulu/">HG Zee Zulu: kit with youtube entry</g-link>
-      </li>
-      <li>
-        <g-link to="/mg/00-gundam-7s-g/">MG 00 Gundam: just a nice kit :D</g-link>
+      <li v-for="entry in $page.entries.edges" :key="entry.url">
+        <g-link :to="entry.node.url">
+          <Thumb :width="200" :height="200" :photoFile="entry.node.cover" />
+          {{ entry.node.date }}: {{ entry.node.kit.title }}
+        </g-link>
       </li>
     </ul>
+    <hr />
+    <small><g-link to="/everything/">...or just see EVERYTHING!</g-link></small>
   </Layout>
 </template>
 
 <script>
+import Thumb from "~/components/Thumb.vue";
+
 export default {
+  components: {
+    Thumb,
+  },
   metaInfo: {
     title: "Welcome to GCU Tactical Grace!",
   },
 };
 </script>
+
+<page-query>
+query allEntries {
+  entries: allEntriesCache(sortBy:"date", limit:3, skip: 3) {
+    edges {
+      node {
+        id
+        date
+        url
+        cover
+        kit {
+          title
+        }
+      }
+    }
+  }
+}
+</page-query>
