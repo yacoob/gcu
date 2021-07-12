a656c125adb9512cc022cfbab97aaf3374deea9e
Jakub Turski <yacoob@gmail.com>
2021-07-12 19:55:47 +0100

    Bring back grade as a separate object.
    More cumbersome to query but easier to do grade pages. Also, grade.id is actually equal to the title which is kind of weird.

--- gridsome.config.js
+++ gridsome.config.js
@@ -13,6 +13,14 @@ module.exports = {
         typeName: 'Kit',
         path: '*/*.yaml',
         baseDir: './kits',
+        refs: {
+          // This field is not present in the file; I set it in
+          // gridsome.server.js via onCreateNode.
+          grade: {
+            typeName: 'Grade',
+            create: true
+          }
+        }
       }
     }
   ],
